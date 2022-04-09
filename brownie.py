c2a29bb95905302507c64e2b8ea07e157c9f90b3
transfaeries <transfaeries@gmail.com>
2022-04-09 16:09:10 -0400

    simplified, added poetry dependency management

--- brownie.py
+++ brownie.py
@@ -1,55 +1,34 @@
-import discord
 import os
-import time
 import logging
-import asyncio
-
-
-
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='>')
+
+logging.basicConfig(level=logging.INFO)
+
+
+DESCRIPTION = """Helpful bot that helps take care of the embassy"""
+
+intents = discord.Intents.default()
+# intents.members = True
+# intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", description=DESCRIPTION, intents=intents)
+
+
+@bot.event
+async def on_ready():
+    """runs when bot is ready"""
+    logging.info(f"Logged in as {bot.user} (ID: {bot.user.id})")
+    logging.info("------")
+
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+async def ping(ctx, *args):
+    """Ping the bot"""
+    if args:
+        return await ctx.send("pong " + " ".join(args))
+    return await ctx.send("pong")
 
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        self.last_message: str = ""
-        print('Logged on as', self.user)
-        logging.getLogger().setLevel("INFO")
-        logging.info('Logged on as %s',self.user)
-
-    async def on_message(self, message: discord.message):
-        """How the bot reacts to being messaged"""
-        logging.info("%s : %s", message.author,message.content)
-        asyncio.sleep(1)
-        # don't respond to ourselves
-        if message.author == self.user:
-            return
-
-        if message.content == self.last_message:
-            return
-
-        self.last_message = message.content
-
-        if message.content == 'ping':
-            await message.channel.send('pong')
-
-        if message.content == '!post':
-            await self.post_a_shitpost(message)
-
-    async def post_a_shitpost(self,message):
-        await message.channel.send("This is a shitpost")
-
-
-
-
-
-
-
-client = MyClient()
-client.run(os.environ['DISCORD_TOKEN'])
+bot.run(os.environ["DISCORD_TOKEN"])
