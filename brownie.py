4dceed11f603dc8d496adfedf01d34e258720465
transfaeries <transfaeries@gmail.com>
2022-04-09 16:38:48 -0400

    ready to put in fly p much

--- brownie.py
+++ brownie.py
@@ -1,5 +1,7 @@
 import os
 import logging
+import random
+
 import discord
 from discord.ext import commands
 
@@ -10,7 +12,7 @@ logging.basicConfig(level=logging.INFO)
 DESCRIPTION = """Helpful bot that helps take care of the embassy"""
 
 intents = discord.Intents.default()
-# intents.members = True
+intents.members = True
 # intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", description=DESCRIPTION, intents=intents)
@@ -29,6 +31,30 @@ async def ping(ctx, *args):
     if args:
         return await ctx.send("pong " + " ".join(args))
     return await ctx.send("pong")
+
+@bot.command()
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
+
+@bot.command()
+async def post(ctx):
+    return await ctx.send("this is a shitpost")
+
+
+@bot.command(description='For when you wanna settle the score some other way')
+async def choose(ctx, *choices: str):
+    """Chooses between multiple choices."""
+    await ctx.send(random.choice(choices))
+    
+
 
 
 bot.run(os.environ["DISCORD_TOKEN"])
