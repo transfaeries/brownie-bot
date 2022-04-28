e8f1bd5f1efc826ad5e1f165a11f4a413b19d348
transfaeries <contact@transfaerie.com>
2022-04-27 23:09:08 -0400

    merged in utils

---
+++ brownie-utils.py
@@ -0,0 +1,26 @@
+import tweepy
+import pluralkit
+import os
+import logging
+import asyncio
+
+pk = pluralkit.Client(os.environ['PK_TOKEN'])
+logging.basicConfig(level=logging.INFO)
+
+class pkHelper:
+    def __init__(self) -> None:
+        pass
+
+    async def main():
+
+        time,front = await pk.get_fronters()
+        if front is None:
+            front = "no one"
+        logging.info(f"{time}: {front} is in front to start with")
+
+        members = pk.get_members()
+        async for member in members:
+            logging.info(f"{member.name} (`{member.id}`)")
+
+# loop = asyncio.get_event_loop()
+# loop.run_until_complete(main())
